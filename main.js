60b317802bc532ff47d5f603812caada8061db27
Samuel Durán <sduniversidad20@gmail.com>
2024-12-31 11:33:36 -0500

    vccc v

--- main.js
+++ main.js
@@ -10,31 +10,18 @@ const server = createServer(app); // Crear servidor HTTP
 const io = new Server(server); // Inicializar Socket.IO con el servidor HTTP
 
 let users = {}; // Almacena la información de las salas y jugadores
-let HOST_SOCKET_ID = null; // Almacena el ID del socket del host
+const HOST_USERNAME = "verhal"; // Nombre de usuario del host
 
 app.use(express.static("public")); // Servir archivos estáticos
 
 io.on("connection", (socket) => {
     console.log("Nuevo usuario conectado:", socket.id);
 
-    socket.on("set-host", () => {
-        // Establecer el socket ID del host
-        if (!HOST_SOCKET_ID) {
-            HOST_SOCKET_ID = socket.id;
-            socket.emit("host-set", "Eres el host de la sala.");
-        } else {
-            socket.emit("host-error", "Ya hay un host establecido.");
-        }
-    });
-
     socket.on("join-room", (username, roomId) => {
         if (!users[roomId]) {
             users[roomId] = {
                 players: {},
                 waitingPlayers: {},
-                inGamePlayers: {},
-                winners: {},
-                lossers: {},
                 host: null,
                 gameStarted: false,
             };
@@ -49,10 +36,11 @@ io.on("connection", (socket) => {
         }
 
         // Si el usuario es el host, permitir la creación de la sala
-        if (socket.id === HOST_SOCKET_ID) {
+        if (username === HOST_USERNAME && !roomData.host) {
             roomData.host = { username, socketID: socket.id };
             socket.join(roomId);
             socket.emit("room-created", roomId); // Notificar al host que la sala fue creada
+            console.log(`${username} ha creado la sala ${roomId}`);
         } else if (roomData.host) {
             // Si la sala ya tiene un host, no permitir que otros la creen
             socket.emit("failed-to-create-room", "La sala ya tiene un host.");
@@ -69,11 +57,12 @@ io.on("connection", (socket) => {
         const roomData = users[roomId];
         const userSocketId = Object.keys(roomData.waitingPlayers).find(id => roomData.waitingPlayers[id] === username);
         
-        if (userSocketId && roomData.host && roomData.host.socketID === socket.id) {
+        if (userSocketId && roomData.host && roomData.host.username === HOST_USERNAME) {
             // Mover al usuario de la lista de espera a la lista de jugadores
             roomData.players[userSocketId] = username;
             delete roomData.waitingPlayers[userSocketId]; // Eliminar de la lista de espera
             io.to(userSocketId).emit("user-accepted", username); // Notificar al usuario que fue aceptado
+            console.log(`${username} ha sido aceptado en la sala ${roomId}`);
         }
     });
 
